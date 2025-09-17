a15284c4d20dd01afb8c02a94534450edf4aa83c
linying-tastien <2227580291@qq.com>
2025-09-17 14:33:53 +0800

    fix: cdn file

--- src/lib/cdn.ts
+++ src/lib/cdn.ts
@@ -1,6 +1,5 @@
 // src/lib/cdn.ts
-const CDN_BASE =
-  'https://cdn.jsdelivr.net/gh/leahlin01/blog-2025@main/public/assets'; // 例如: https://cdn.jsdelivr.net/gh/<user>/<repo>@<commit>/public
+const CDN_BASE = 'https://cdn.jsdelivr.net/gh/leahlin01/blog-2025@main/public'; // 例如: https://cdn.jsdelivr.net/gh/<user>/<repo>@<commit>/public
 
 export function cdn(path: string) {
   if (!CDN_BASE) return path; // 未配置时走本地路径（/images/xxx.png）
